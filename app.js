4b86a3b3c669519333ca0628a2a749e4f93a331f
fiacattozzo <fiacattozzo@gmail.com>
2025-09-13 20:18:56 -0300

    estilicé el footer y el botón de contacto final

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const nextButton = document.querySelector('.next-btn');
-const video = document.querySelector('.hero-video');
 
-const movieList = ['/videos/video1.mp4', '/videos/video2.mp4', '/videos/video3.mp4'];
-
-let index = 0;
-nextButton.addEventListener('click', function() {
-    index+= 1;
-    video.src = movieList[index];
-
-    if (index === 3){
-        index = -1;
-    }
-
-})
