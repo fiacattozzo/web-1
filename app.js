1c7a48fadfbc5a8a01cd9128c1eceb38453f6c98
fiacattozzo <fiacattozzo@gmail.com>
2025-09-13 21:24:01 -0300

    sumé sección servicios

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.service-item');
+  const previewImg = document.getElementById('service-preview-img');
 
+  items.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      const imgsrc = item.getAttribute('data-preview');
+      previewImg.src = imgsrc;
+      previewImg.style.opacity = '1';
+    });
+
+    item.addEventListener('mouseleave', () => {
+      previewImg.style.opacity = '0';
+    });
+  });
+});
